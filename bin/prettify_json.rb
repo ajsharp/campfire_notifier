45bd184ee9f37e46b4d81c9cc986e99efa149a0b
Alex Sharp <ajsharp@gmail.com>
2009-12-18 00:31:44 -0800

    removed previously vendored binaries

--- bin/prettify_json.rb
+++
@@ -1,3 +0,0 @@
-#!/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/ruby
-require File.join(File.dirname(__FILE__), "../vendor/gems/environment")
-load File.join(File.dirname(__FILE__), "../vendor/gems/gems/json_pure-1.2.0/bin/prettify_json.rb")
