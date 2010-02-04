e10cf4c2d420d48717464ae6751addf15d24da65
Alex Sharp <ajsharp@gmail.com>
2010-02-04 02:54:15 -0800

    added mock_settings spec helper method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,3 +15,9 @@ fixtures = File.dirname(File.expand_path(__FILE__)) + "/fixtures"
 FakeWeb.allow_net_connect = false
 FakeWeb.register_uri(:get, "http://example.campfirenow.com/rooms.json", :body => File.read("#{fixtures}/rooms.json"))
 
+Spec::Runner.configure do |config|
+  def mock_settings(opts = {})
+    { 'token' => 'token', 'subdomain' => 'example', 'room_id' => 111111}.merge(opts)
+  end
+
+end
